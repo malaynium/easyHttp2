f7de09ede890c391612482bfe1b9b6dc04791b1a
wanlab-io <hello.wanlab@gmail.com>
2020-05-03 10:55:11 +0800

    delete request method

--- easyhttp.js
+++ easyhttp.js
@@ -39,38 +39,38 @@ class EasyHTTP {
   // }
 
   // HTTP Put Request
-  put( url, data ){
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'PUT',
+  // put( url, data ){
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+
+  //       body: JSON.stringify(data)
+  //     })
+
+  //     .then(res => res.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err));
+  //   });
+  // }
+
+ // HTTP DELEE Request
+ delete(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'DELETE',
         headers: {
           'Content-type': 'application/json'
         },
+    })
 
-        body: JSON.stringify(data)
-      })
-
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-    });
-  }
-
-//  // HTTP DELEE Request
-//  delete(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url, {
-//       method: 'DELETE',
-//         headers: {
-//           'Content-type': 'application/json'
-//         },
-//     })
-
-//     .then(res => res.json())
-//     .then(() => resolve('Resource Deleted...'))
-//     .catch(err => reject(err)); 
-//   });
-// }
+    .then(res => res.json())
+    .then(() => resolve('Resource Deleted...'))
+    .catch(err => reject(err)); 
+  });
+}
 
 } // end of class
 
