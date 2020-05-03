307098b90ed2791d63b7f4c41bcf2438aaca0209
wanlab-io <hello.wanlab@gmail.com>
2020-05-03 10:54:25 +0800

    post request method

--- easyhttp.js
+++ easyhttp.js
@@ -20,11 +20,29 @@ class EasyHTTP {
   //   });
   // }
 
-  // HTTP Post Request
-  post( url, data ){
+  // // HTTP Post Request
+  // post( url, data ){
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+
+  //       body: JSON.stringify(data)
+  //     })
+
+  //     .then(res => res.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err));
+  //   });
+  // }
+
+  // HTTP Put Request
+  put( url, data ){
     return new Promise((resolve, reject) => {
       fetch(url, {
-        method: 'POST',
+        method: 'PUT',
         headers: {
           'Content-type': 'application/json'
         },
@@ -37,24 +55,6 @@ class EasyHTTP {
       .catch(err => reject(err));
     });
   }
-
-//   // HTTP Put Request
-//   put( url, data ){
-//     return new Promise((resolve, reject) => {
-//       fetch(url, {
-//         method: 'PUT',
-//         headers: {
-//           'Content-type': 'application/json'
-//         },
-
-//         body: JSON.stringify(data)
-//       })
-
-//       .then(res => res.json())
-//       .then(data => resolve(data))
-//       .catch(err => reject(err));
-//     });
-//   }
 
 //  // HTTP DELEE Request
 //  delete(url) {
